be2d0d66e1296da1dc14b9f239419de978745422
amamchych <amamchych@softserveinc.com>
2013-12-24 21:46:05 +0200

    Added example how to use LCD1602 thought PCF8574 I2C Interface expander with New LiquidCrystal library

---
+++ lcd1602/i2c_Lcd/HelloWorld_i2c/HelloWorld_i2c.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#define BACKLIGHT_PIN 13
+
+//LiquidCrystal_I2C lcd(0x27, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+int interval = 0;
+
+void setup()
+{
+  //Switch on the backlight
+  //pinMode(BACKLIGHT_PIN, OUTPUT );
+  //digitalWrite(BACKLIGHT_PIN, HIGH );
+
+  lcd.begin(16,2);
+  lcd.home();
+  //lcd.clear(); 
+  lcd.setCursor(0, 0);
+  lcd.print("Serial LCD");
+  lcd.setCursor(1, 1);
+  lcd.print("count:");
+  lcd.setCursor(8, 1);
+  lcd.print(interval);
+}
+
+void loop()
+{
+  delay(500);
+  interval++;
+  lcd.setCursor(8, 1);
+  lcd.print(interval);
+}
